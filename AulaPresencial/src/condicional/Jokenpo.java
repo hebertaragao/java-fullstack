354b45a5c4b3c689de7356bdb9e5cebca3f9683d
hebertaragao <hebert.aragao@outlook.com>
2025-05-29 14:21:46 -0300

    created jokenpo game

---
+++ AulaPresencial/src/condicional/Jokenpo.java
@@ -0,0 +1,34 @@
+package condicional;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jokenpo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random random = new Random();
+		
+		String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+		System.out.println("Escolha: Pedra, Papel ou Tesoura?");
+		String escolhaJogador = scan.nextLine();
+		
+		int indiceComputador = random.nextInt(3);
+		String escolhaComputador = opcoes[indiceComputador];
+		
+		System.out.println("O Computaodr escolheu: " + escolhaComputador);
+		
+		if(escolhaJogador.equalsIgnoreCase(escolhaComputador)) {
+			System.err.println("Empate!");
+		} else if ((escolhaJogador.equalsIgnoreCase("Pedra") && escolhaComputador.equals("Tesoura")) ||
+                (escolhaJogador.equalsIgnoreCase("Papel") && escolhaComputador.equals("Pedra")) ||
+                (escolhaJogador.equalsIgnoreCase("Tesoura") && escolhaComputador.equals("Papel"))) {
+         System.out.println("Você ganhou!");
+		} else {
+			System.out.println("Você perdeu!");
+		}
+		scan.close();
+
+	}
+
+}
