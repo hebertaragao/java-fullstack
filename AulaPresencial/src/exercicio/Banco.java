30c4d4f82b12ba1e631eec75c90c7ea8ba1fa82b
hebertaragao <hebert.aragao@outlook.com>
2025-06-03 20:50:45 -0300

    created class Banco.java

---
+++ AulaPresencial/src/exercicio/Banco.java
@@ -0,0 +1,23 @@
+package exercicio;
+
+public class Banco {
+
+	public static void main(String[] args) {
+		/*
+		 * O usuário começa com um saldo inicial e
+		 * pode escolher entre as opções:
+		 * 1 - Depositar
+		 * 2 - Sacar
+		 * 3 - Transferir
+		 * 4 - Sair
+		 * 
+		 * O programa deve validar que não é possivel
+		 * transferir ou sacar mais do que o saldo disponível.
+		 * Após cada operação, o saldo atualizado deve ser exibido.
+		 * O programa só deve encerrar quando o usuário escolher
+		 * a opção 4 - Sair.
+		 * */
+
+	}
+
+}
