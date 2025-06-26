33c05218c342bc0b74bc20c16e7562fb7eafd686
hebertaragao <hebert.aragao@outlook.com>
2025-06-26 19:26:20 -0300

    Created project ExercicioDeLacoDeRepeticao, pakage ecerciciolacoderepeticao and class Exercicio1.java

---
+++ ExerciciosDeLacoDeRepeticao/src/exerciciodelacoderepeticao/Exercicio1.java
@@ -0,0 +1,12 @@
+package exerciciodelacoderepeticao;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        
+        // Contagem regressiva de 10 até 1
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Contagem regressiva: " + i);
+        }
+    }
+}
