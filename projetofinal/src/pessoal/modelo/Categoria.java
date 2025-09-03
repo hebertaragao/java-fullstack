ab5181a8fdfa2261c765d6237ae4b5201bd0f329
hebertaragao <hebertaragao@outlook.com>
2025-09-03 20:42:56 -0300

    Create project projetofinal

--- projetofinal/src/pessoal/modelo/Categoria.java
+++ projetofinal/src/pessoal/modelo/Categoria.java
@@ -1,5 +1,24 @@
 package pessoal.modelo;
 
 public class Categoria {
+	
+	private String nome;
+	
+	public Categoria(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome() {
+		this.nome = nome;
+	}
+	
+	@Override
+	public String toString() {
+		return nome;
+	}
 
 }
