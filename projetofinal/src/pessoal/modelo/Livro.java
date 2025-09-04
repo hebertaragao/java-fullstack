872ef0f455995b949b4d53e0f14f52dfd1ce75e8
hebertaragao <hebertaragao@outlook.com>
2025-09-03 21:36:20 -0300

    Update class Livro.java

--- projetofinal/src/pessoal/modelo/Livro.java
+++ projetofinal/src/pessoal/modelo/Livro.java
@@ -1,5 +1,37 @@
 package pessoal.modelo;
+public class Livro extends Item {
+	private String isbn;
+	private int anoPublicacao;
 
-public class Livro {
+	public Livro(String titulo, String autorArtista, Categoria categoria, String isbn, int anoPublicacao) {
+		super(titulo, autorArtista, categoria);
+		this.isbn = isbn;
+		this.anoPublicacao = anoPublicacao;
+	}
 
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	@Override
+	public String getTipo() {
+		return "Livro";
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Tipo: Livro, ISBN: " + isbn + ", Ano: " + anoPublicacao;
+	}
 }
