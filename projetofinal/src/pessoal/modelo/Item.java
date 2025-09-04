2f56f7af1d4c2255ac45e8f4a6c87dba9946518c
hebertaragao <hebertaragao@outlook.com>
2025-09-03 21:03:41 -0300

    Update class Item.java

--- projetofinal/src/pessoal/modelo/Item.java
+++ projetofinal/src/pessoal/modelo/Item.java
@@ -1,5 +1,46 @@
 package pessoal.modelo;
 
-public class Item {
+public abstract class Item {
+	
+	private String titulo;
+	private String autorArtista;
+	private Categoria categoria;
+	
+	public Item(String titulo, String autorArtista, Categoria categoria) {
+		this.titulo = titulo;
+		this.autorArtista = autorArtista;
+		this.categoria = categoria;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo() {
+		this.titulo = titulo;
+	}
+	
+	public String getAutorArtista() {
+		return autorArtista;
+	}
+	
+	public void setAutorArtista() {
+		this.autorArtista = autorArtista;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria() {
+		this.categoria = categoria;
+	}
+	
+	public abstract String getTipo();
+	
+	@Override
+	public String toString() {
+		return "Titulo: " + titulo + ", Autor/Artista: " + autorArtista + ", Categoria: " + categoria.getNome();
+	}
 
 }
