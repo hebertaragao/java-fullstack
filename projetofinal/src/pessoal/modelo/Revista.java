ee7a2562125c43f74e201c96c2cd4dcd5c284aaf
hebertaragao <hebert.aragao@outlook.com>
2025-09-04 23:37:29 -0300

    Update class Revista.java

--- projetofinal/src/pessoal/modelo/Revista.java
+++ projetofinal/src/pessoal/modelo/Revista.java
@@ -1,5 +1,38 @@
 package pessoal.modelo;
 
-public class Revista {
+public class Revista extends Item {
+	private int numeroEdicao;
+	private String dataPublicacao;
 
+	public Revista(String titulo, String autorArtista, Categoria categoria, int numeroEdicao, String dataPublicacao) {
+		super(titulo, autorArtista, categoria);
+		this.numeroEdicao = numeroEdicao;
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	public int getNumeroEdicao() {
+		return numeroEdicao;
+	}
+
+	public void setNumeroEdicao(int numeroEdicao) {
+		this.numeroEdicao = numeroEdicao;
+	}
+
+	public String getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(String dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	@Override
+	public String getTipo() {
+		return "Revista";
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Tipo: Revista, Edição: " + numeroEdicao + ", Data: " + dataPublicacao;
+	}
 }
