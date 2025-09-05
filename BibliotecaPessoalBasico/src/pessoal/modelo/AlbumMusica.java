f446f66d3e9d1f04b645a108a1c69fb824c93ae0
hebertaragao <hebert.aragao@outlook.com>
2025-09-04 23:56:01 -0300

    Update class AlbumMusica.java

--- BibliotecaPessoalBasico/src/pessoal/modelo/AlbumMusica.java
+++ BibliotecaPessoalBasico/src/pessoal/modelo/AlbumMusica.java
@@ -1,5 +1,39 @@
 package pessoal.modelo;
 
-public class AlbumMusica {
+public class AlbumMusica extends Item {
+	private int numeroFaixas;
+	private String genero;
+
+	public AlbumMusica(String titulo, String autorArtista, Categoria categoria, int numeroFaixas, String genero) {
+		super(titulo, autorArtista, categoria);
+		this.numeroFaixas = numeroFaixas;
+		this.genero = genero;
+	}
+
+	public int getNumeroFaixas() {
+		return numeroFaixas;
+	}
+
+	public void setNumeroFaixas(int numeroFaixas) {
+		this.numeroFaixas = numeroFaixas;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public String getTipo() {
+		return "Album de Música";
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + ", Tipo: Álbum de Música, Faixas: " + numeroFaixas + ", Gênero: " + genero;
+	}
 
 }
