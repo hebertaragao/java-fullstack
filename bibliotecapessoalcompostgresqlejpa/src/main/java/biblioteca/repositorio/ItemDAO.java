68f4b31ce34af2fdf621ad6ce812e96db9441350
hebertaragao <hebert.aragao@outlook.com>
2025-09-17 14:48:37 -0300

    Created interface ItemDAO.java

---
+++ bibliotecapessoalcompostgresqlejpa/src/main/java/biblioteca/repositorio/ItemDAO.java
@@ -0,0 +1,14 @@
+package biblioteca.repositorio;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import biblioteca.modelo.Item;
+
+public interface ItemDAO {
+	
+	void adicionarItem(Item item) throws SQLException;
+	Item buscarItemPorId(int id) throws SQLException;
+	List<Item> listarTodosItens() throws SQLException;
+	void removerItem(int id) throws SQLException;
+}
