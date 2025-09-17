c9c6c23d8013bce5abadff01eee578d61a1ecd8d
hebertaragao <hebert.aragao@outlook.com>
2025-09-17 14:17:43 -0300

    Created class Categoria.java

--- bibliotecapessoalcompostgresqlejpa/src/main/java/biblioteca/modelo/Item.java
+++ bibliotecapessoalcompostgresqlejpa/src/main/java/biblioteca/modelo/Item.java
@@ -1,5 +1,66 @@
 package biblioteca.modelo;
 
-public class Item {
+import java.time.LocalDate;
 
+public abstract class Item {
+	protected int id;
+	protected String titulo;
+	protected String autorArtista;
+	protected Categoria categoria;
+	protected LocalDate dataCadastro;
+
+	public Item(String titulo, String autorArtista, Categoria categoria) {
+		this.titulo = titulo;
+		this.autorArtista = autorArtista;
+		this.categoria = categoria;
+		this.dataCadastro = LocalDate.now();
+	}
+
+// Getters e Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutorArtista() {
+		return autorArtista;
+	}
+
+	public void setAutorArtista(String autorArtista) {
+		this.autorArtista = autorArtista;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + id + ", Título: \'" + titulo + "\'\n" + "Autor/Artista: \'" + autorArtista + "\'\n"
+				+ "Categoria: " + (categoria != null ? categoria.getNome() : "N/A") + "\n" + "Data de Cadastro: "
+				+ dataCadastro;
+	}
 }
