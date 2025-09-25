b1ca0a432428b6fd6f4b4f12a3940ed1b029e45e
hebertaragao <hebertaragao.dev@gmail.com>
2025-09-25 20:53:40 -0300

    Created componets LivrosPage

---
+++ livraria_front_presencial/src/app/components/livros-page/livros-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livros-page',
+  imports: [],
+  templateUrl: './livros-page.html',
+  styleUrl: './livros-page.css'
+})
+export class LivrosPage {
+
+}
