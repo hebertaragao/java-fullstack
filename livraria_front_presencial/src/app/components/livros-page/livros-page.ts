7cd9b7baa991ec6c331dfceb8a8ee9ae54a3251c
hebertaragao <hebertaragao.dev@gmail.com>
2025-09-25 21:34:10 -0300

    Update LivrosPage

--- livraria_front_presencial/src/app/components/livros-page/livros-page.ts
+++ livraria_front_presencial/src/app/components/livros-page/livros-page.ts
@@ -1,4 +1,12 @@
+import { identifierName } from '@angular/compiler';
 import { Component } from '@angular/core';
+
+interface Livro{
+  id: number;
+  titulo: string;
+  autor: string;
+  texto: string;
+}
 
 @Component({
   selector: 'app-livros-page',
@@ -7,5 +15,42 @@ import { Component } from '@angular/core';
   styleUrl: './livros-page.css'
 })
 export class LivrosPage {
+  novoLivro: Omit<Livro, 'id'> = {
+    autor: "",
+    titulo: "",
+    texto: "",
+  }
 
-}
+  private novoId = 1;
+  
+  livros: Livro[] = [
+    {
+     id: 1,
+      titulo: 'Dom Casmurro',
+      autor: 'Machado de Assis',
+      texto: 'Uma das principais obras da literatura brasileira, que narra a história de Bentinho e sua obsessão por Capitu, questionando temas como ciúme, traição e a complexidade das relações humanas.'
+    },
+    {
+      id: 2,  
+      titulo: 'O Cortiço',
+      autor: 'Aluísio Azevedo',
+      texto: 'Romance naturalista que retrata a vida em um cortiço no Rio de Janeiro do século XIX, explorando as condições sociais e os instintos humanos através de personagens marcantes como João Romão e Bertoleza.'
+    },
+    {
+      id: 3,
+      titulo: 'Iracema',
+      autor: 'José de Alencar',
+      texto: 'Lenda do Ceará que conta a história de amor entre a índia Iracema e o português Martim, representando o encontro entre as culturas indígena e europeia no Brasil colonial.'
+    },
+    {
+      id: 4,
+      titulo: 'Memórias Póstumas de Brás Cubas',
+      autor: 'Machado de Assis',
+      texto: 'Romance inovador narrado por um defunto autor, que conta sua vida de forma irônica e filosófica, abordando temas como a sociedade brasileira do século XIX, vaidade e hipocrisia humana.'
+    },
+  ];
+  cadastrarLivro(){}
+  editarLivro(livroId: number){}
+  excluirLivro(livroId: number){}
+  }
+
