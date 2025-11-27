9f0904fd54af42dd02c6b7fcfa403db982b99346
hebertaragao <hebertaragao.dev@gmail.com>
2025-11-27 19:51:30 -0300

    Created Packages controllers, repositories and services, class CategoriaRepository and LivroRepository

---
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/repositories/LivroRepository.java
@@ -0,0 +1,10 @@
+package br.com.fuctura.bibliotecanoite.repositories;
+
+import br.com.fuctura.bibliotecanoite.models.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
