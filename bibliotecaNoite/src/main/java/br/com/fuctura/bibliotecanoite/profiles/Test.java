d2273921f7a777c45dd3cc28a23065d46773e275
hebertaragao <hebertaragao.dev@gmail.com>
2025-11-27 21:20:19 -0300

    Rename class Test for TestProfile

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/profiles/Test.java
+++
@@ -1,4 +0,0 @@
-package br.com.fuctura.bibliotecanoite.profiles;
-
-public class Test {
-}
---
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/profiles/TestProfile.java
@@ -0,0 +1,12 @@
+package br.com.fuctura.bibliotecanoite.profiles;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("teste")
+public class TestProfile {
+    
+
+}
