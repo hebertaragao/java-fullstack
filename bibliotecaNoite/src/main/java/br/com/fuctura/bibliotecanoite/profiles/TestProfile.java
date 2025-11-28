dc95abe1807bb75368279b398c7f977b75fdbaff
hebertaragao <hebertaragao.dev@gmail.com>
2025-11-27 21:28:23 -0300

    Created class DevProfile and application-dev-propeties

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/profiles/TestProfile.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/profiles/TestProfile.java
@@ -1,12 +1,22 @@
 package br.com.fuctura.bibliotecanoite.profiles;
 
 
+import br.com.fuctura.bibliotecanoite.services.DBService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("teste")
 public class TestProfile {
-    
+
+    @Autowired
+    private DBService dbService;
+
+    @PostConstruct
+    public void instanciaDB(){
+        this.dbService.instanciaDB();
+    }
 
 }
