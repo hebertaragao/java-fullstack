aff16fd91d681346f9971e29dd9cb088fc69d396
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-02 19:51:22 -0300

    Update CategoriaService

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/services/CategoriaService.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/services/CategoriaService.java
@@ -1,7 +1,14 @@
 package br.com.fuctura.bibliotecanoite.services;
 
+import br.com.fuctura.bibliotecanoite.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    
 }
