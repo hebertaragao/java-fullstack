7ba7648d5a72312348e98723ae77c475af565c48
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-02 19:58:48 -0300

    Update CategoriaController

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/controllers/CategoriaController.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/controllers/CategoriaController.java
@@ -1,4 +1,8 @@
 package br.com.fuctura.bibliotecanoite.controllers;
 
+import br.com.fuctura.bibliotecanoite.models.Categoria;
+
 public interface CategoriaController {
+
+    Categoria findById(Integer id);
 }
--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/services/CategoriaService.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/services/CategoriaService.java
@@ -1,8 +1,11 @@
 package br.com.fuctura.bibliotecanoite.services;
 
+import br.com.fuctura.bibliotecanoite.models.Categoria;
 import br.com.fuctura.bibliotecanoite.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -10,5 +13,8 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    
+    public Categoria findById(Integer id) {
+       Optional <Categoria> categoria = categoriaRepository.findById(id);
+       return categoria.get();
+    }
 }
