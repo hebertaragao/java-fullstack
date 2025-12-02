e4c5fde9d33644485f83f1dbff64880ebe919ef3
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-02 20:28:33 -0300

    Update CategoriaControllerimpl

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/controllers/CategoriaController.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/controllers/CategoriaController.java
@@ -2,7 +2,13 @@ package br.com.fuctura.bibliotecanoite.controllers;
 
 import br.com.fuctura.bibliotecanoite.models.Categoria;
 
+import java.util.List;
+
 public interface CategoriaController {
 
     Categoria findById(Integer id);
+    List<Categoria> findAll();
+    Categoria update (Integer id, Categoria categoria);
+    void delete (Integer id);
+
 }
--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/controllers/impl/CategoriaControllerImpl.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/controllers/impl/CategoriaControllerImpl.java
@@ -1,4 +1,30 @@
 package br.com.fuctura.bibliotecanoite.controllers.impl;
 
-public class CategoriaControllerImpl {
+import br.com.fuctura.bibliotecanoite.controllers.CategoriaController;
+import br.com.fuctura.bibliotecanoite.models.Categoria;
+
+import java.util.List;
+
+public class CategoriaControllerImpl implements CategoriaController {
+
+
+    @Override
+    public Categoria findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Categoria> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Categoria update(Integer id, Categoria categoria) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
 }
