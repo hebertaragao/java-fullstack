110eb0001ec69663b3994f20ba25cf4cfaf4106f
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-02 21:03:31 -0300

    Update CategoriaService

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/services/CategoriaService.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/services/CategoriaService.java
@@ -5,6 +5,7 @@ import br.com.fuctura.bibliotecanoite.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,5 +17,9 @@ public class CategoriaService {
     public Categoria findById(Integer id) {
        Optional <Categoria> categoria = categoriaRepository.findById(id);
        return categoria.get();
+    }
+
+    public List<Categoria> findAll() {
+        return categoriaRepository.findAll();
     }
 }
