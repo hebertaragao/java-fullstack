ebd1be3a39796f411274be68e450851601d4d9f6
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-04 20:59:02 -0300

    Update Class ModelMapperConfig and CategoriaControllerImpl CategoriaController

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/config/ModelMapperConfig.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/config/ModelMapperConfig.java
@@ -1,8 +1,15 @@
 package br.com.fuctura.bibliotecanoite.config;
 
+import jakarta.annotation.PostConstruct;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class modelMapperConfig {
-    
+public class ModelMapperConfig {
+
+    @PostConstruct
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
