f2e3602534daacf42eea40f2819dfe50997c4160
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-09 20:00:06 -0300

    Update DevProfile application.propeties and application-dev.propeties

--- bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/profiles/DevProfile.java
+++ bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecanoite/profiles/DevProfile.java
@@ -1,9 +1,19 @@
 package br.com.fuctura.bibliotecanoite.profiles;
 
+import br.com.fuctura.bibliotecanoite.services.DBService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("dev")
 public class DevProfile {
+    @Autowired
+    private DBService dbService;
+
+    @PostConstruct
+    public void instanciaDB() {
+        this.dbService.instanciaDB();
+    }
 }
